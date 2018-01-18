9a33a3ddb9e941dba32f4b108ef41b597af0fcce
theafergie <a.ferguson86@yahoo.com>
2018-01-18 14:30:27 -0600

    2.2 complete

---
+++ ch02/Date.java
@@ -0,0 +1,20 @@
+public class Date
+{
+    public static void main(String args[]){
+        System.out.println("Date Exercise");
+
+        String day = "Thursday";
+        int date = 18;
+        String month = "January";
+        int year = 2018;
+
+        System.out.println("American format:");
+        System.out.println(day + ", " + month + " " + date + ", " + year);
+
+        System.out.println("European format:");
+        System.out.println(day + " " + date + " " + month + " " + year);
+
+    }
+
+
+}
