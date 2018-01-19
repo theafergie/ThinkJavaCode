722fb9e70cc36efca72a4fb1d4f14c727845d739
theafergie <a.ferguson86@yahoo.com>
2018-01-19 16:13:55 -0600

    4.2b complete

--- ch04/SimpleMethods.java
+++ ch04/SimpleMethods.java
@@ -1,11 +1,25 @@
 public class SimpleMethods
 {
-    public static void main(String [] args)
+    public static void main(String[] args)
     {
         printCount(5);
+        printSum(7, 13);
+
+        printSum(4, 6);
+        printSum(7, 2);
     }
+
     private static void printCount(int count)
     {
         System.out.println("This count is: " + count);
+    }
+
+    private static void printSum(int a, int b)
+    {
+        System.out.print(a);
+        System.out.print(" + ");
+        System.out.print(b);
+        System.out.print(" = ");
+        System.out.println(a + b);
     }
 }
