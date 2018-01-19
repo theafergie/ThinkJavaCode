07f2c3ce9b78f2c8e85e66a416fcf38f09159ecc
theafergie <a.ferguson86@yahoo.com>
2018-01-18 19:38:09 -0600

    2.3 Revised

--- ch02/Time.java
+++ ch02/Time.java
@@ -5,23 +5,34 @@ public class Time
         System.out.println("Time 2.3");
         int hour = 15;
         int minute = 4;
-        int second = (hour * 60 + minute) * 60;
+        //int second = (hour * 60 + minute) * 60;
+        int second = 16;
 
-        System.out.print("Number of seconds since midnight: ");
-        System.out.println(second);
+        int secondsPerMinute = 60;
+        int minutesPerHour = 60;
 
-        System.out.print("Number of seconds remaining in the day: ");
-        System.out.println(86400 - second);
+        int secondsSinceMidnight = (hour * minutesPerHour * secondsPerMinute) +(minute * secondsPerMinute) + second;
 
-        System.out.print("Percentage of the day that has passed: ");
-        System.out.println(second * 100 / 86400);
+
+        System.out.println("Number of seconds since midnight: " + secondsSinceMidnight);
+
+        int hoursPerDay = 24;
+        int secondsInDay = hoursPerDay * minutesPerHour * secondsPerMinute;
+        int secondsRemainingInDay = secondsInDay - secondsSinceMidnight;
+        System.out.println("Number of seconds remaining in the day: " + secondsRemainingInDay);
+
+        int percentageDayPassed = secondsSinceMidnight * 100 / secondsInDay;
+        System.out.println("Percentage of the day that has passed: " + percentageDayPassed);
+
 
         int hour2 = 15;
         int minute2 = 55;
-        int second2 = (hour2 * 60 + minute2) * 60;
+        int second2 = 25;
 
-        System.out.print("Time elapsed in seconds since starting: ");
-        System.out.println(second2 - second);
+        int endingTime = (hour2 * minutesPerHour * secondsPerMinute) + (minute2 * secondsPerMinute) + second2;
+
+        int timeElapsed = endingTime - secondsSinceMidnight;
+        System.out.print("Time elapsed in seconds since starting: " + timeElapsed);
 
     }
 }
