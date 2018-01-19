33e0454e24fdc002002870bd28bb06a7f8a137b0
theafergie <a.ferguson86@yahoo.com>
2018-01-18 19:58:50 -0600

    2.4 Completed

---
+++ ch02/IntExtremes.java
@@ -0,0 +1,20 @@
+public class IntExtremes
+{
+    public static void main(String arg[])
+    {
+
+        System.out.println("Exercise 2.4");
+        int positiveInt = 2147483647;
+
+        System.out.println(positiveInt);
+
+        positiveInt = positiveInt + 1;
+        System.out.println(positiveInt);
+
+        int negativeInt = -2147483648;
+        System.out.println(negativeInt);
+
+        negativeInt = negativeInt - 1;
+        System.out.println(negativeInt);
+    }
+}
