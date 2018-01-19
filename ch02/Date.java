dcb3505dda312ff62ddeef80c266a43507c14436
theafergie <a.ferguson86@yahoo.com>
2018-01-19 16:24:36 -0600

    4.3 Completed

--- ch02/Date.java
+++ ch02/Date.java
@@ -1,20 +1,37 @@
 public class Date
 {
-    public static void main(String args[]){
-        System.out.println("Date Exercise");
-
-        String day = "Thursday";
-        int date = 18;
-        String month = "January";
-        int year = 2018;
+    public static void main(String args[])
+    {
+        System.out.println("Date Exercise 2.2/4.3");
 
         System.out.println("American format:");
-        System.out.println(day + ", " + month + " " + date + ", " + year);
+        printAmerican("Friday", 19, "January", 2018);
 
         System.out.println("European format:");
-        System.out.println(day + " " + date + " " + month + " " + year);
-
+        printEuropean("Thursday", 19, "January", 2018);
     }
 
+    private static void printAmerican(String day, int date, String month, int year)
+    {
+        System.out.print(day);
+        System.out.print(", ");
+        System.out.print(month);
+        System.out.print(" ");
+        System.out.print(date);
+        System.out.print(", ");
+        System.out.print(year);
+        System.out.println();
+    }
 
+    private static void printEuropean(String day, int date, String month, int year)
+    {
+        System.out.print(day);
+        System.out.print(" ");
+        System.out.print(month);
+        System.out.print(" ");
+        System.out.print(date);
+        System.out.print(" ");
+        System.out.print(year);
+        System.out.println();
+    }
 }
