9192e9644c25eb052a6220be180ccca16ae95f1c
theafergie <a.ferguson86@yahoo.com>
2018-01-18 20:24:03 -0600

    2.6 Completed

---
+++ ch02/DoubleByZero.java
@@ -0,0 +1,16 @@
+public class DoubleByZero
+{
+    public static void main(String args[])
+    {
+
+        System.out.println("Exercise 2.6");
+
+        double a = 42;
+        double b = 0;
+        double result;
+
+        result = a / b;
+
+        System.out.println(result);
+    }
+}
