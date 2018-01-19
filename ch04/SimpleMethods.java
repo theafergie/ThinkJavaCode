918dc5a32e9dc1c8f915bdb7e9edffb7233a0c5f
theafergie <a.ferguson86@yahoo.com>
2018-01-19 15:39:38 -0600

    4.2a complete

---
+++ ch04/SimpleMethods.java
@@ -0,0 +1,11 @@
+public class SimpleMethods
+{
+    public static void main(String [] args)
+    {
+        printCount(5);
+    }
+    private static void printCount(int count)
+    {
+        System.out.println("This count is: " + count);
+    }
+}
