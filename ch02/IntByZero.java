a807cccbf8d39e38689d619603a6b37ea5dddb85
theafergie <a.ferguson86@yahoo.com>
2018-01-18 20:16:50 -0600

    2.5 Completed-Exception error

---
+++ ch02/IntByZero.java
@@ -0,0 +1,14 @@
+public class IntByZero
+{
+    public static void main(String args[])
+    {
+
+        int a = 42;
+        int b = 0;
+        int result;
+
+        result = a / b;
+
+        System.out.println(result);
+    }
+}
