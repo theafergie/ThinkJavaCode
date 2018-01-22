a4710141072fb019a9347959e227acf710db9840
theafergie <a.ferguson86@yahoo.com>
2018-01-22 11:00:59 -0600

    5-B Complete

--- ch05/LogicMethods.java
+++ ch05/LogicMethods.java
@@ -7,6 +7,10 @@ public class LogicMethods
         printIsLarge(99);
         printIsLarge(100);
         printIsLarge(200);
+
+        printIsLargeOrSmall(1);
+        printIsLargeOrSmall(50);
+        printIsLargeOrSmall(195);
     }
 
     private static void printIsLarge(int number)
@@ -21,6 +25,21 @@ public class LogicMethods
         }
         System.out.println();
 
+    }
+    private static void printIsLargeOrSmall(int number)
+    {
+        System.out.println("printIsLargeOrSmall");
+        System.out.println("Number is: " + number);
+
+        if (number > 99)
+        {
+            System.out.println("The number is large");
+        }
+        else if (number < 10)
+        {
+            System.out.println("The number is small");
+        }
+        System.out.println();
     }
 }
 
