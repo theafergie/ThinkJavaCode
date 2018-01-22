9e6ac120a403ae7daf150c6445a6cb4e9049f84d
theafergie <a.ferguson86@yahoo.com>
2018-01-22 12:01:39 -0600

    5-D Complete

--- ch05/LogicMethods.java
+++ ch05/LogicMethods.java
@@ -13,8 +13,14 @@ public class LogicMethods
         printIsLargeOrSmall(195);
 
         printLargest(10, 15);
-        printLargest(20,2);
-        printLargest(5,5);
+        printLargest(20, 2);
+        printLargest(5, 5);
+
+        printLargestOdd(9,3);
+        printLargestOdd(9,57);
+        printLargestOdd(6,4);
+        printLargestOdd(7,7);
+
 
     }
 
@@ -40,8 +46,7 @@ public class LogicMethods
         if (number > 99)
         {
             System.out.println("The number is large");
-        }
-        else if (number < 10)
+        } else if (number < 10)
         {
             System.out.println("The number is small");
         }
@@ -59,10 +64,36 @@ public class LogicMethods
         else if (number1 < number2)
         {
             System.out.println("The largest number is: " + number2);
+        } else {
+            System.out.println("The numbers are equal");
+        }
+        System.out.println();
+    }
+
+    private static void printLargestOdd(int number1, int number2)
+    {
+        int remainder1 = number1 % 2;
+        int remainder2 = number2 % 2;
+
+        System.out.println("printLargestOdd");
+        System.out.println("The numbers are: " + number1 + ", " + number2);
+
+        if (number1 > number2 && remainder1 == 1)
+        {
+            System.out.println("The largest odd number is: " + number1);
+        }
+        else if (number1 < number2 && remainder2 == 1)
+        {
+            System.out.println("The largest odd number is: " + number2);
+        }
+        else if (remainder1 == 0 && remainder2 == 0)
+        {
+            System.out.println("Neither number is odd");
         }
         else
         {
-            System.out.println("The numbers are equal");
+            System.out.println("Two odds make an even");
+            System.out.println("The sum is: " + (number1 + number2));
         }
         System.out.println();
     }
