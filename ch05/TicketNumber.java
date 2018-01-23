af5d6549905a7f4c401bb6aff403cadbed143260
theafergie <a.ferguson86@yahoo.com>
2018-01-22 21:16:27 -0600

    5-F Complete

---
+++ ch05/TicketNumber.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import java.util.Scanner;
+
+public class TicketNumber
+{
+    public static void main(String[] args)
+    {
+        Scanner user = new Scanner(System.in);
+
+        System.out.println("Enter ticket number: ");
+        int ticketNumber;
+        ticketNumber = user.nextInt();
+
+        int lastDigit;
+        lastDigit = ticketNumber % 10;
+        System.out.println("lastDigit: " + lastDigit);
+
+        int ticketPrefix = ticketNumber - (lastDigit * 10);
+        System.out.println("ticketPrefix: " + ticketPrefix);
+
+        int remainder = ticketPrefix % 7;
+        System.out.println("Remainder is: " + remainder);
+
+        if (remainder == lastDigit)
+        {
+            System.out.println("Good Number");
+        }
+        else
+        {
+            System.out.println("Bad Number");
+        }
+    }
+}
