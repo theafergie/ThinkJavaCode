36cff363994f27a4567566378843bd068a58b696
theafergie <a.ferguson86@yahoo.com>
2018-01-23 10:19:44 -0600

    6.4 Complete

--- ch06/ValueMethods.java
+++ ch06/ValueMethods.java
@@ -11,6 +11,11 @@ public class ValueMethods
         boolean isATriangle = isTriangle(12, 6, 6);
         System.out.println(isATriangle);
 
+        System.out.println();
+
+        double testMultadd = multadd(1.0, 2.0, 3.0);
+        System.out.println(testMultadd);
+
 
     }
     public static boolean isDivisible(int n, int m)
@@ -38,5 +43,11 @@ public class ValueMethods
             threeSides = true;
         }
         return threeSides;
+    }
+
+    public static double multadd(double a, double b, double c)
+    {
+        double add = a * b + c;
+        return add;
     }
 }
