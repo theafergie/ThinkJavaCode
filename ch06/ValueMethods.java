0b0f3a4c6f3e2adf6f45a7bfb5cf3b475ca7df89
theafergie <a.ferguson86@yahoo.com>
2018-01-23 10:10:54 -0600

    6.3 Complete

--- ch06/ValueMethods.java
+++ ch06/ValueMethods.java
@@ -6,6 +6,11 @@ public class ValueMethods
         boolean isItDivisible = isDivisible(10, 5);
         System.out.println(isItDivisible);
 
+        System.out.println();
+
+        boolean isATriangle = isTriangle(12, 6, 6);
+        System.out.println(isATriangle);
+
 
     }
     public static boolean isDivisible(int n, int m)
@@ -21,5 +26,17 @@ public class ValueMethods
         }
         return divisible;
     }
-
+    public static boolean isTriangle(int a, int b, int c)
+    {
+        boolean threeSides;
+        if (a + b < c || b + c < a || a + c < b)
+        {
+            threeSides = false;
+        }
+        else
+        {
+            threeSides = true;
+        }
+        return threeSides;
+    }
 }
