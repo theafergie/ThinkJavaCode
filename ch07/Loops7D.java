2970c234b0a03e6b0543c6791fb798aaad8f141b
theafergie <a.ferguson86@yahoo.com>
2018-01-24 14:18:26 -0600

    7.D complete

---
+++ ch07/Loops7D.java
@@ -0,0 +1,16 @@
+public class Loops7D
+{
+    public static void  main(String[] args)
+    {
+        sevenCForLoop(1);
+    }
+
+    public static void sevenCForLoop(int x)
+    {
+        while(x <=31)
+        {
+            System.out.println(x);
+            x++;
+        }
+    }
+}
