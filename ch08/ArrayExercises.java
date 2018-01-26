0983f0e3335ea826f312365dcc06ee41764535bc
theafergie <a.ferguson86@yahoo.com>
2018-01-26 11:59:24 -0600

    Exercise 8.C

--- ch08/ArrayExercises.java
+++ ch08/ArrayExercises.java
@@ -2,18 +2,21 @@ public class ArrayExercises
 {
     public static void  main(String[] args)
     {
-//        int values[] = {1,2,3,4};
-//        printArray(values);
+        int values[] = {1,2,3,4};
+        printArray(values);
 
         int eightB[] = {10, 2, 3, 5};
         arrayTotal(eightB);
+
+        int eightC[] = {10,40,15,2};
+        arrayMax(eightC);
     }
 
     private static void printArray(int[] values)
     {
         for(int value : values)
         {
-            System.out.println("{ " + value + " }");
+            System.out.print(value);
         }
     }
     private static void arrayTotal(int[] values)
@@ -23,12 +26,19 @@ public class ArrayExercises
         {
             total += values[i];
         }
-        System.out.println("The sum of elements: " + total);
-
-
+        System.out.println("\nThe sum of elements: " + total);
     }
-//    private static int arrayMax(int[] vaules)
-//    {
-//
-//    }
+    private static void arrayMax(int[] values)
+    {
+        int max = values[0];
+
+        for(int i = 0; i < values.length; i++)
+        {
+            if(values[i] > max)
+            {
+                max = values[i];
+            }
+        }
+        System.out.println("Max number is: " + max);
+    }
 }
