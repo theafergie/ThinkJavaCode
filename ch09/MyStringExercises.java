bab55179099429dec24a612c98f1b77eb0b5e4d1
theafergie <a.ferguson86@yahoo.com>
2018-01-29 14:40:11 -0600

    9.B  complete

--- ch09/MyStringExercises.java
+++ ch09/MyStringExercises.java
@@ -5,11 +5,21 @@ public class MyStringExercises
         printFirstCharacter("Hello");
         printFirstCharacter("Goodbye");
 
+        printLastCharacter("Hello");
+        printLastCharacter("Goodbye");
+
     }
 
     private static void printFirstCharacter(String first)
     {
         System.out.println("Print the first character in " + first);
         System.out.println(first.charAt(0));
+    }
+
+    private static void printLastCharacter(String last)
+    {
+        int length = last.length();
+        System.out.println("Print the last character in " + last);
+        System.out.println(last.charAt(length - 1));
     }
 }
